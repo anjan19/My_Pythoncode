ced6cae733d6eb2f9b900a82c693a95d3d48b223
Anjaneyulu Budidha <41720457+anjan19@users.noreply.github.com>
2018-10-04 11:58:41 +0530

    update
    update

--- Retail_Store.py
+++ Retail_Store.py
@@ -1,7 +1,8 @@
+
 def data_read_file():
     d_list=[]
     d_dict={}
-    with open('C:/Python34/fruits.txt','r') as fh:
+    with open('D:\Anjan\GCP\PythonProject\FileToWrite.txt','r') as fh:
     
         for line in fh:
             if line.split(',')[0] in d_dict:
@@ -16,7 +17,7 @@ def data_read_file():
 
 def data_write_file(i_dict):
     d_dict=i_dict
-    with open('C:/Python34/fruits.txt','w') as fh:           
+    with open('D:\Anjan\GCP\PythonProject\FileToWrite.txt','w') as fh:           
         for k,v in d_dict.items():
             for d in v:
                 for k1,v1 in d.items():
@@ -44,8 +45,45 @@ def add_item(itm_typ,itm_nm,itm_prc):
     else:
         add_dict[itm_typ]=[{itm_nm:itm_prc}]
         print(itm_nm+' with '+itm_typ+' item category is successfully added to store')   
-    data_write_file(add_dict)    
-add_item('vegetable','tomoto',10.0) 
+    data_write_file(add_dict)
+
+def update_item(itm_typ,itm_nm,itm_prc):
+    add_dict=data_read_file()
+    add_lst=[]
+    itm_chk=False
+    if itm_typ in add_dict:
+        add_lst=add_dict[itm_typ]
+        for dct in add_lst:
+            if itm_nm in dct:
+                itm_chk=True
+                add_lst.remove(dct)
+                add_lst.append({itm_nm:itm_prc})
+                add_dict[itm_typ]=add_lst
+                print(itm_nm+': is successfully updated in store')
+        if itm_chk == False:
+            print(itm_nm+': is not availbale in store,Check and provide correct item name')
+    else:
+        print(itm_typ+': This item category is not available in  store')
+    data_write_file(add_dict)
+
+update_item('phone','redmi1',3500.0)     
         
-            
-        
+def delete_item(itm_typ,itm_nm,itm_prc):
+    add_dict=data_read_file()
+    add_lst=[]
+    itm_chk=False
+    if itm_typ in add_dict:
+        add_lst=add_dict[itm_typ]
+        for dct in add_lst:
+            if itm_nm in dct:
+                itm_chk=True
+                add_lst.remove(dct)
+                add_dict[itm_typ]=add_lst
+                print(itm_nm+': is successfully deleted from store')
+        if itm_chk == False:
+            print(itm_nm+': is not availbale in store,Check and provide correct item name')
+    else:
+        print(itm_typ+': This item category is not available in  store')
+    data_write_file(add_dict)
+    
+delete_item('vegetable','tomoto',20.0) 
