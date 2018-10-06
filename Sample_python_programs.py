7b769248748add11773369782b7d722ea29f86cf
Anjaneyulu Budidha <41720457+anjan19@users.noreply.github.com>
2018-10-06 15:20:25 +0530

    Update and rename Python (2).py to Sample_python_programs.py
    updated

--- Sample_python_programs.py
+++ Sample_python_programs.py
@@ -1,74 +1,25 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
+# Sample function1
 def ordnum(a,b):
     if a >  b:
         return [a,b]
     else:
         return [b,a]
 
-
-# In[3]:
-
-
-ordnum(1,5)
-
-
-# In[4]:
-
-
-def sq(x): return x*x
-
-
-# In[8]:
-
-
+# Sample function2, Lambda function
 def mymap(a,f):
     r=[]
     for i in a:
         v=f(i)
         r.append(v)
     return r
-a=[2,3,4]
-
-
-# In[10]:
-
-
-mymap(a,sq)
-
-
-# In[11]:
-
 
 mymap([2,3,6,7],lambda x: x*3)
 
-
-# In[18]:
-
-
+# Lambda example 2
 f=lambda x,y:print('we are x and y',str(x)+str(y))
 f(1,2)
 
-
-# In[32]:
-
-
-a=[2,3,4]
-b='anjan'
-#for i in a:
-    #print(i)
-#for i in range(0,len(b)):
-    #print(b[i])
-for i in range(len(b)):
-    print("Happy Bday to: ",b[i])
-
-
-# In[39]:
-
+# Dictionary and accessing keys and values
 
 an=dict()
 an['anjan']=34
@@ -78,16 +29,8 @@ print(an)
 d2={'shamp': 23,'brush':26,'surf':56}
 print(d2)
 
-
-# In[42]:
-
-
 print(an.keys())
 print(an.values())
-
-
-# In[3]:
-
 
 lst=['anjan','ravi','anjan','sree','anjan','sree','sam']
 names=dict()
@@ -103,15 +46,7 @@ for i in names:
 for key,val in names.items():
     print(key,val)
 
-
-# In[57]:
-
-
-
-
-
-# In[16]:
-
+# File opening, reading
 
 wcline=open('wc.txt')
 wcount=dict()
@@ -128,48 +63,17 @@ for key,val in wcount.items():
         bigval=val
 print(bigwrd,bigval)
 
-
-# In[14]:
-
-
-fhand=open('wc.txt')
-#for line in fhand:
-    #print(line)
-line=fhand.read()
-wlst=line.split()
-print(wlst)
-
-
-# In[18]:
-
-
-max(wcount)
-
-
-# In[19]:
-
-
+# Tuple 
 t=('a','b','c')
 print(t)
-
-
-# In[22]:
-
 
 for i in t:
     print(i)
 
-
-# In[24]:
-
-
 (a,b)=(99,100)
 print(b)
 
-
-# In[42]:
-
-
+#Function on tuple
 def order(x,y):
     if x > y:
         return (x,y)
@@ -178,10 +82,7 @@ def order(x,y):
 (a,b)=order(2,1)
 print(a,b)
 
-
-# In[49]:
-
-
+# Program to count word occurance 
 file=input('Enter the file name: ')
 words={}
 lst=[]
@@ -197,18 +98,4 @@ lst.sort(reverse=True)
 #print(lst)
 for v,k in lst[:10]:
     print(k,v)
-
-
-# In[1]:
-
-
-d={'anjan':34,'ravi':2}
-for i in d:
-    print(i)
-
-
-# In[2]:
-
-
-(0,1,200000)<(0,3,4)
 
